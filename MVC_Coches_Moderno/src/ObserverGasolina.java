27613f4c50c811ed9467480b3af0d8c494aecb5c
Adrianmc94 <amiguezcampos@danielcastelao.org>
2025-06-04 13:21:24 +0200

    ObserverGasolina hecho

--- MVC_Coches_Moderno/src/ObserverGasolina.java
+++ MVC_Coches_Moderno/src/ObserverGasolina.java
@@ -1,2 +1,10 @@
-public class ObserverGasolina {
+public class ObserverGasolina implements Observer {
+
+    @Override
+    public void update(Coche coche) {
+        if ( coche.gasolina < 10){
+            System.out.println("OBSERVER! te estas quedando sin gasolina!");
+        }
+
+    }
 }
